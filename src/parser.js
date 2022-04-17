9360659ca8a96e2966880f330e7a5282268e799b
Aliaxander Novikov <nerevarin11@gmail.com>
2022-04-17 21:53:21 +0300

    correction errors #2

--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,15 @@
-import { uniqueId } from 'lodash';
+import _ from 'lodash';
 
 export default (data) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data.contents, 'text/xml');
-  const feedId = uniqueId();
+  const feedId = _.uniqueId();
   const feedTitle = doc.querySelector('title');
   const feedDesc = doc.querySelector('description');
   const feedLink = doc.querySelector('link');
   const items = doc.querySelectorAll('item');
   const posts = [...items].map((item) => {
-    const postId = uniqueId();
+    const postId = _.uniqueId();
     const postTitle = item.querySelector('title');
     const postDesc = item.querySelector('description');
     const postLink = item.querySelector('link');
