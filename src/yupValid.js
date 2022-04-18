6ed87f864c420544dfdef1d320874a89d32870a4
Aliaxander Novikov <nerevarin11@gmail.com>
2022-04-18 14:35:38 +0300

    correction errors #7

--- src/yupValid.js
+++ src/yupValid.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export default (url, i18n, state) => {
-  const schema = yup.string().url(i18n.t('feedback.invalid')).notOneOf(state.requests).required();
+  const schema = yup.string().url(i18n.t('feedback.invalid')).notOneOf([state.requests, i18n.t('feedback.repeat')]).required();
 
   return schema.validate(url);
 };
