74ac70070f04576caa99fb90ee1115e8a79b0110
Aliaxander Novikov <nerevarin11@gmail.com>
2022-04-18 12:28:11 +0300

    correction errors #6

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+export default {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
