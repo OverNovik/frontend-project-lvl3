d76a391b934d29119a3bdd7091eb1a19cf6a8311
Aliaxander Novikov <nerevarin11@gmail.com>
2022-04-18 17:20:20 +0300

    correction errors #7

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-import path from 'path';
+import path, { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
   entry: './index.js',
