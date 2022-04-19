0a1e29d808931565cc783008535435254bd2a1bd
Aliaxander Novikov <nerevarin11@gmail.com>
2022-04-20 01:30:34 +0300

    correction errors #7

--- src/getLink.js
+++ src/getLink.js
@@ -1,6 +1,6 @@
 export default (url) => {
-  const newUrl = new URL(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`);
+  const newUrl = new URL('https://allorigins.hexlet.app/get?url=');
   newUrl.searchParams.set('disableCache', true);
   newUrl.searchParams.set('url', url);
-  return newUrl.toString();
+  return newUrl;
 };
