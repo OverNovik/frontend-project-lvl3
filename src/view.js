359c05c4a912530b41cf2f5eaf1bfd668e88153b
Aliaxander Novikov <nerevarin11@gmail.com>
2022-04-22 13:56:42 +0300

    correction errors #9

--- src/view.js
+++ src/view.js
@@ -2,7 +2,7 @@ import renderFeeds from './components/feeds.js';
 import renderPosts from './components/posts.js';
 
 export default (i18n, form, input, feedbackArea) => {
-  const btn = form.querySelector('btn-lg');
+  const btn = document.querySelector('.btn-lg');
   const watched = (path, value) => {
     console.log('value', value);
     console.log('path', path);
@@ -29,12 +29,15 @@ export default (i18n, form, input, feedbackArea) => {
       input.classList.add('is-invalid');
       feedbackArea.classList.add('text-danger');
       feedbackArea.textContent = i18n.t('feedback.networkError');
-    } else if (path === 'form.stateForm' && value === 'load') {
-      input.setAttribute('readonly', 'readonly');
-      btn.setAttribute('disabled', true);
-    } else if (path === 'form.stateForm' && value === 'success') {
-      input.removeAttribute('readonly');
-      btn.setAttribute('disabled', false);
+    } else if (path === 'form.status') {
+      if (value === 'load') {
+        input.setAttribute('readonly', 'readonly');
+        btn.setAttribute('disabled', true);
+      }
+      if (value === 'success') {
+        input.removeAttribute('readonly');
+        btn.removeAttribute('disabled');
+      }
     } else if (path === 'form.error') {
       feedbackArea.classList.remove('text-success');
       input.classList.add('is-invalid');
